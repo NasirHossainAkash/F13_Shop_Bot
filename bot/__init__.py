7ce70f99356a58d7b66663337480a470a23b0b8c
NasirHossainAkash <79259896+NasirHossainAkash@users.noreply.github.com>
2023-02-21 17:27:44 +0600

    added env support

--- bot/__init__.py
+++ bot/__init__.py
@@ -9,9 +9,15 @@ logger = logging.getLogger(__name__)
 from telegram import ParseMode
 from telegram.ext import Defaults, Updater
 
-ADMIN_ID = 1301921132
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+if not ADMIN_ID:
+    ADMIN_ID = 1301921132
+
+
 TOKEN = os.getenv("TOKEN")
-TOKEN = "5472393791:AAFTwZUtvv0vt70-rINcVUp1SGMzCESSN3o"
+if not TOKEN:
+    TOKEN = "5472393791:AAFTwZUtvv0vt70-rINcVUp1SGMzCESSN3o"
 if not TOKEN:
     raise Exception("Token Not found on environment var")
 defaults = Defaults(parse_mode=ParseMode.HTML, run_async=True)
